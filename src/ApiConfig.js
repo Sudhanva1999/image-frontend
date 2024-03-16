5814df6862ce25ea2254b78b487f122ca9a9d8d2
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-03-16 00:05:30 -0400

    Ad

--- src/ApiConfig.js
+++ src/ApiConfig.js
@@ -1,5 +1,5 @@
 // API base URL
-export const API_BASE_URL = 'http://ec2-18-216-36-63.us-east-2.compute.amazonaws.com:5000';
+export const API_BASE_URL = 'https://ec2-18-216-36-63.us-east-2.compute.amazonaws.com:5000';
 
 // API endpoints
 export const API_ENDPOINTS = {
